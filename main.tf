6d8e40e9b725f26b9ceb0d98191967ca463cf3d1
Gowri-Kumar <gowriskkumar@gmail.com>
2024-08-09 09:47:48 +1200

    test

--- main.tf
+++ main.tf
@@ -13,3 +13,56 @@ terraform {
 provider "azurerm" {
   features {}
 }
+
+resource "azurerm_resource_group" "main" {
+  name     = "learn_terraform_lin"
+  location = "australiasoutheast"
+}
+
+
+resource "azurerm_virtual_network" "main"{
+  name = "learn_terraform_lin_vnet"
+  location = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  address_space = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "main"{
+  name = "learn_terraform_lin_subnet"
+  resource_group_name = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes = ["10.0.0.0/24"]
+}
+
+resource "azurerm_network_interface" "internal"{
+  name = "learn_terraform_lin_network_interface_internal"
+  resource_group_name = azurerm_resource_group.main.name
+  location = azurerm_resource_group.main.location
+
+  ip_configuration {
+    name = "internal"
+    subnet_id = azurerm_subnet.main.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "main" {
+  name = "learn-lin-vm"
+  resource_group_name = azurerm_resource_group.main.name
+  location = azurerm_resource_group.main.location
+  size = "Standard_B1s"
+  admin_username = "user.admin"
+  admin_password = "admin.password@12"  
+  network_interface_ids = [azurerm_network_interface.internal.id]
+  os_disk {
+    caching = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+  source_image_reference {
+    publisher = "MicrosoftwindowsServer"
+    offer = "WindowsServer"
+    sku = "2016-DataCenter"
+    version = "latest"
+  }
+
+}
